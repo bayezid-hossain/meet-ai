3fab2a9fc3300b58dcc0a3a3700065ce8cfeede5
Bayezid <arkantos.tas@gmail.com>
2025-06-02 13:14:51 +0600

    01: Setup added: [ shadcn all components ]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <div className="text-4xl font-bold text-green-500">
-      hello
-    </div>
-  );
+  return <Button>Hello</Button>;
 }

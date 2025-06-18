0460aa21c9d56b8c9022b132e3c0de6b2adce677
Bayezid <arkantos.tas@gmail.com>
2025-06-18 15:23:19 +0600

    11: agents form

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,10 +1,28 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
+import { agentInsertSchema } from "../schema";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-    getMany: baseProcedure.query(async () => {
+    // TODO:change 'getMany' to use 'protectedProcedure'
+    getMany: protectedProcedure.query(async () => {
         const data = await db.select().from(agents)
         return data;
+    }),
+    // TODO:change 'getOne' to use 'protectedProcedure'
+
+    getOne: protectedProcedure.input(z.object({
+        id: z.string()
+    })).query(async ({ input }) => {
+        const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id))
+        return existingAgent;
+    }),
+
+    create: protectedProcedure.input(agentInsertSchema).mutation(async ({ input, ctx }) => {
+
+        const [createdAgent] = await db.insert(agents).values({ ...input, userId: ctx.auth.user.id }).returning();
+        return createdAgent;
     })
 })
